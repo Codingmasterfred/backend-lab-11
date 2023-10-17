6bbd8dd3a539fefaa4b5b07daf6a49db6f1d7363
Codingmasterfred <fredrickg777@gmail.com>
2023-10-16 20:18:49 -0700

    backend

--- server.js
+++ server.js
@@ -22,14 +22,9 @@ const verifyUser = require('./verifyUser'); // Import the verifyUser middleware 
 const app = express();
 
 
-app.use(cors());
+app.use(cors({ origin: 'https://can-of-books-frontend-xvf4.onrender.com' }));
 app.use(express.json())
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'https://can-of-books-frontend-xvf4.onrender.com');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
-  });
+
 const PORT = process.env.PORT || 3001;
 
 // const verifyJWT = jwt({
